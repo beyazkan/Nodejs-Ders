1053b2f361a494e7aba8bca2a24bfc79880d474d
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-11-24 10:37:24 +0300

    ES ve CommonJS import işlemleri

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 // Local Modülleri
 // ES - CommonJS
-var deneme = require("Deneme");
+//import data from "./users/userData.js"
+//import {userData, time, printName} from "./users/userData.js";
+// import * as data from "./users/userData.js";
+//import {data1 as a, data2 as b, data3 as c} from "./users/userData.js";
+
+// console.log(userData);
+// console.log(time);
+// printName();
+// console.log(data);
+// console.log(data.printName());
+// console.log(data.time);
+// console.log(data.userData);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// express -> CommonJS
+// node-fetch@3 -> ES
+// Third Party Modüller
+// import fetch from "node-fetch"
+// fetch("");
+// import express from "express"
+
+// express.Router;
+
+var a = 5;
+if(a == 5){
+    var response = await import("./users/userData.js");
+    console.log(response.IntValue);
+}
--- users/userData.js
+++ users/userData.js
@@ -2,3 +2,16 @@
 
 
 // Default Exports
+//export default {name: "Ali Eren", age: 19};
+// export var userData = {name: "Ali Eren", age: 19}, time = Date.now();
+// export function printName(){
+//     console.log("Ali Eren");
+// }
+
+// var data1 = 10;
+// var data2 = 20;
+// var data3 = 30;
+
+// export {data1, data2, data3};
+
+export var IntValue = 5;
