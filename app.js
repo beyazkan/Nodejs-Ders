782335cb4001a91db01900c8017a425d09d3ecfc
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-11-24 14:15:49 +0300

    Events - Olayların Kullanımı

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
-// Local Modülleri
-// ES - CommonJS
-//import data from "./users/userData.js"
-//import {userData, time, printName} from "./users/userData.js";
-// import * as data from "./users/userData.js";
-//import {data1 as a, data2 as b, data3 as c} from "./users/userData.js";
+// events
+import {EventEmitter} from "events";
 
-// console.log(userData);
-// console.log(time);
-// printName();
-// console.log(data);
-// console.log(data.printName());
-// console.log(data.time);
-// console.log(data.userData);
+var emitter = new EventEmitter();
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-// express -> CommonJS
-// node-fetch@3 -> ES
-// Third Party Modüller
-// import fetch from "node-fetch"
-// fetch("");
-// import express from "express"
-
-// express.Router;
-
-var a = 5;
-if(a == 5){
-    var response = await import("./users/userData.js");
-    console.log(response.IntValue);
+function clicked(a, b){
+    console.log("Clicked!", a, b);
 }
+
+function clicked2(){
+    console.log("Clicked 2!");
+}
+function clicked3(){
+    console.log("Clicked 3!");
+}
+
+// emitter.addListener("click", clicked);
+// emitter.addListener("click", clicked2);
+//emitter.on("click", clicked);
+// emitter.addListener("click", clicked);
+// emitter.addListener("click", clicked);
+
+// emitter.emit("click", 3, 5);
+// emitter.emit("click", 3, 5);
+// emitter.emit("click", 3, 5);
+// emitter.emit("click", 3, 5);
+// emitter.emit("click", 3, 5);
+
+// emitter.on("click", clicked);
+// emitter.on("click", clicked2);
+// Olayın bir kez çalıştırılması için "once" methodu kullanılır.
+// emitter.once("tek", () => console.log("Tek çağırıldı!"));
+
+// setInterval(()=>{
+//     emitter.emit("click", 3, 5);
+// }, 1000);
+
+// Olayların isimlerini öğrenmek.
+// console.log(emitter.eventNames());
+// Dinleyicilerin fonksiyon isimlerini öğrenmek.
+// console.log(emitter.listeners("click"));
+// Kaç dinleyici olduğunu öğrenmek
+// console.log(emitter.listenerCount("click"));
+
+// Events sayısı sınırı - 10
+// sayı sınırını değiştirmek
+// emitter.setMaxListeners(15);
+// emitter.on("click", clicked);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+// emitter.on("click", clicked2);
+
+// Listenerlerin tek tek silinmesi
+emitter.on("click", () => {console.log("Clicked!")});
+emitter.on("click", clicked2);
+emitter.on("click", clicked3);
+
+console.log(emitter.listeners("click"));
+emitter.removeListener("click", clicked2);
+emitter.off("click", emitter.listeners("click")[0]);
+console.log(emitter.listeners("click"));
+// Toplu silinmesi
+emitter.removeAllListeners("click");
+console.log(emitter.listeners("click"));
