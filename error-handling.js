14f5d1469682ae3a06a69ff165a934a07cd055d4
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-12-04 16:23:20 +0300

    Error handling

---
+++ error-handling.js
@@ -0,0 +1,35 @@
+// Operational Errors - Programmer Errors
+// Throw - Callback - Promise Rejection - EventEmitter
+
+// Error Object
+// var error = new Error("Test Hatası!");
+// console.log(error.message, error.name);
+// Hatanın detayı vererek gösteriyor...
+// console.log(error.stack);
+
+// RangeError
+//var a = 3.14;
+// console.log(a.toFixed(200));
+// console.log(a.toFixed(1));
+
+// ReferenceError
+// function test(){
+//     var a = 3;
+// }
+// test();
+// console.log(a);
+
+// TypeError
+// var obje = {"name": "Ali Eren"};
+// console.log(obje.age.a);
+
+// SyntaxError
+// .
+// console.log(Math.max(1, 3, 5);
+
+// Throw Kullanımı
+
+// throw new Error("Test Hatası");
+// console.log("a");
+
+// Try-Catch-Finally
