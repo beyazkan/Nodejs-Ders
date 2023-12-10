ff4bdaf781832aaa18585328520063690ae1a8e8
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-12-10 08:07:35 +0300

    Garbage Collector - Devam

--- GarbageCollector.js
+++ GarbageCollector.js
@@ -1,4 +1,51 @@
 // var - let - const
+// var a = 5;
+// let a = 5;
+// const a = 5;
 
-var a = 5;
-console.log(a);
+// console.log(a);
+
+// var - local değişken tanımlama
+// function test(){
+//     var a = 5;
+//     console.log(a);
+//     return a;
+// }
+
+// var a = test();
+// console.log(a);
+
+// if(true){
+//     var a = 5;
+// }
+
+// let ile aynı isimden değişken tanımlanamaz
+// if(true){
+//     let a = 5;
+//     let a = 6;
+// }
+
+// const ise let gibi fakat değeri değiştirilmez.
+// if(true){
+//     const a = 5;
+//     a = 10;
+// }
+// console.log(a);
+
+// const obj = {
+//     "a": 1
+// }
+// obj = {
+//     "b": 2
+// }
+// obj["a"] = 2
+
+// console.log(obj);
+
+// const arr = [1, 2, 3];
+// arr.push(5);
+
+// console.log(arr);
+
+// Garbage Collector - Memory Management - Memory Leak
+const a = 5;
