a4dc82c672a6e3b0a03a40694e3a9075563f35e8
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-12-19 15:35:34 +0300

    Regex Dersi

--- RegexExpress.js
+++ RegexExpress.js
@@ -3,3 +3,16 @@
 // E-Posta örneği : adiniz.soyadiniz@hotmail.com
 // Telefon örneği : +905001112233
 // replace - replaceAll - split
+
+var text = "atestasd test asdtest";
+
+// var pattern = /test/g;
+
+// for(let value of text.matchAll(pattern)){
+//     console.log(value);
+// }
+
+var pattern = /test/;
+
+// Bulursa bulunan index, bulamazsa -1 değeri veriyor.
+console.log(text.search(pattern));
